b0ca7412a8220de33d08e45474827b786eb08d64
Mustafa Sevgi <msevgi@netas.com.tr>
2014-12-18 11:06:06 +0200

    Takip edilenler ve takip edenler merge edildi.

--- app/src/main/java/com/msevgi/repost/fragment/FragmentLikedPosts.java
+++ app/src/main/java/com/msevgi/repost/fragment/FragmentLikedPosts.java
@@ -1,11 +1,24 @@
 package com.msevgi.repost.fragment;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.msevgi.repost.R;
+import com.msevgi.repost.constant.ApplicationConstants;
+import com.msevgi.repost.task.GetLikedPosts;
 
 public class FragmentLikedPosts extends BaseFragment {
     @NonNull
     @Override
     protected int getLayoutResource() {
-        return 0;
+        return R.layout.fragment_liked_posts;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        new GetLikedPosts(ApplicationConstants.getLikedPostsUrl()).execute();
     }
 }
